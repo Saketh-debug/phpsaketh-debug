b9ab698000e1b80e0cc4bdf50bffd0a5fafd13a1
Saketh <72593220+Saketh-debug@users.noreply.github.com>
2020-12-06 20:53:41 +0530

    chat

--- chat/index.php
+++ chat/index.php
@@ -6,33 +6,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     
     <title>Chat</title>
-    <style>
-        *                               { margin: 0; padding: 0; }
-body                            { font: 12px "Roboto", Sans-Serif; background: url(https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=827&q=80); }
-h2                              { color: #fa9f00; font-family: 30px 'Roboto', Sans-Serif; margin: 0 0 10px 0; }
-#page-wrap                      { width: 500px; margin: 30px auto; position: relative; }
-
-#chat-wrap                      { border: 1px solid #eee; margin: 0 0 15px 0; }
-#chat-area                      { height: 300px; overflow: auto; border: 1px solid #666; padding: 20px; background: white; }
-#chat-area span                 { color: white; background: #333; padding: 4px 8px; -moz-border-radius: 5px; -webkit-border-radius: 8px; margin: 0 5px 0 0; }
-#chat-area p                    { padding: 8px 0; border-bottom: 1px solid #ccc; }
-
-#name-area                      { position: absolute; top: 12px; right: 0; color: white; font: bold 12px "Lucida Grande", Sans-Serif; text-align: right; }   
-#name-area span                 { color: #fa9f00; }
-
-#send-message-area p            { float: left; color: white; padding-top: 27px; font-size: 14px; }
-#sendie                         { border: 3px solid #999; width: 360px; padding: 10px; font: 12px "Lucida Grande", Sans-Serif; float: right; }
-
-
-
-
-
-
-    </style>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     
-    <!-- <link rel="stylesheet" href="style.css" type="text/css" /> -->
+    <link rel="stylesheet" href="style.css" type="text/css" />
     
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script type="text/javascript" src="chat.js"></script>
