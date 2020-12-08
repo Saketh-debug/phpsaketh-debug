fa1590a582537d64df63013a432fedfe23b39754
Hrutvik Palutla <72236971+Hrutvik-Palutla@users.noreply.github.com>
2020-12-07 20:27:12 -0600

    removed unneccessary icons at the bottom

--- contacts.php
+++ contacts.php
@@ -90,7 +90,7 @@ body{
           <p class="leading-normal my-5">Zentech
             <br> Amazing, revolutionary and unprecedented, as always
           </p>
-          <span class="inline-flex">
+          <!--<span class="inline-flex">
             <a class="text-gray-500">
               <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -112,7 +112,7 @@ body{
                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
               </svg>
             </a>
-          </span>
+          </span>-->
         </div>
       </div>
     </div>
