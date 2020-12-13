87431e5d9111375d057770a8e2e0a55ec355f9f8
Hrutvik Palutla <72236971+Hrutvik-Palutla@users.noreply.github.com>
2020-12-12 19:16:17 -0600

    Update birthday.php

--- birthday.php
+++ birthday.php
@@ -100,7 +100,7 @@
                     <p>Saketh is one of the co-founders of Zentech, and its CEO.</p>
                     <p>
                       Today, the 13th of December, 2020, we celebrate Saketh's 14<sup>th</sup> birthday.
-                      <p>
+                      <p style = "font-style: italic;">
                       I would like to thank Saketh for all that he as contributed and given to our company: 
                       the creation and editing of the website, and also for his fun and amazing games.
                       Many many happy returns of the day, and hope you have a good year ahead! Happy birthday, Saketh.
