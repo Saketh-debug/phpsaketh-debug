7658ca47eccb37a607b507190168bd5dbbc42de0
Saketh <72593220+Saketh-debug@users.noreply.github.com>
2021-01-21 23:40:37 +0530

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact Us | Zentech</title>
   <head profile="http://www.w3.org/2005/10/profile">
-      <link rel="icon" 
-            type="image/png" 
-            href="Images/Zentech Logo.png" />
+    <link rel="icon" 
+          type="image/png" 
+          href="Zentech Logo.png" />
   <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
@@ -34,37 +34,38 @@ body{
 </head>
 <body>
   <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light"id="n1">
-    <a class="navbar-brand" href="index.php">Zentech</a>
+    <a class="navbar-brand" href="index">Zentech</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="projects.php">Projects</a>
+          <a class="nav-link" href="projects">Projects</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Youtube.php">Youtube</a>
+          <a class="nav-link" href="Youtube">Youtube</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Game.php">Saketh's Box Run</a>
+          <a class="nav-link" href="Game">Saketh's Box Run</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Game2.php">Saketh's Ghost Run</a>
+          <a class="nav-link" href="Game2">Saketh's Ghost Run</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="#">Contact Us <span class="sr-only">(current)</span></a>
         </li>
-        <!--<li class="nav-item">
-          <a class="nav-link" href="birthday.php">Birthday Celebrations</a>
-        </li>-->
         <li class="nav-item">
-          <a class="nav-link" href="dd.php">Art of CSS</a>
+          <a class="nav-link" href="about">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dd">Art of CSS</a>
         </li>
       </ul>
     </div>
   </nav>
   <section class="text-gray-700 body-font relative">
+    <form form action="https://formspree.io/f/myybojog" method="POST">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
@@ -73,24 +74,23 @@ body{
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-1/2">
-          <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text">
+          <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" name="name">
         </div>
         <div class="p-2 w-1/2">
-          <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Email" type="email">
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="useremail">
         </div>
         <div class="p-2 w-full">
-          <textarea class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block" placeholder="Message"></textarea>
+          <textarea class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block" placeholder="Message" type="message" name="User message"></textarea>
         </div>
-        <button type="button" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" data-toggle="modal" data-target="#exampleModal">
-          Submit
-        </button>
+        <input type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" data-toggle="modal" data-target="#exampleModal" value="Submit" name="Submit">
+    
         
         <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-          <p>Or else, mail to: <a id="mail" href = "mailto:zentechcorporation@gmail.com">zentechcorporation@gmail.com</a></p>
+          <p>Or else, mail to: <a class="text-indigo-500" id="mail" href = ""></a></p>
           <p class="leading-normal my-5">Zentech
             <br> Amazing, revolutionary and unprecedented, as always
           </p>
-          <!--<span class="inline-flex">
+          <span class="inline-flex">
             <a class="text-gray-500">
               <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -112,17 +112,18 @@ body{
                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
               </svg>
             </a>
-          </span>-->
+          </span>
         </div>
       </div>
     </div>
   </div>
+</form>
 </section>
 <!-- Button trigger modal -->
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -132,14 +133,14 @@ body{
         </button>
       </div>
       <div class="modal-body">
-        Message sent. However, if you want a faster response, you can send an email to <a href = "mailto:zentechcorporation@gmail.com">zentechcorporation@gmail.com</a>.
+        Message sent. However, if you want a faster response, you can send an email to <a href = ""></a>.
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>       
       </div>
     </div>
   </div>
-</div>
+</div> -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
