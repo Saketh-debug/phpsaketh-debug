7cec63c46598bc1ef7ce1766df5389d09372eb08
Saketh <saketh_p2006@outlook.com>
2021-02-07 23:24:43 +0530

    Hrutvik's birthday
    Many many happy returns of the day.

---
+++ hrutvik'sbirthday.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      
+      ga('create', 'UA-XXXXX-Y', 'auto');
+      ga('send', 'pageview');
+      </script>
+    <meta charset="utf-8">
+    <meta name="google-site-verification" content="Y0Tmy7ijLWTzXG2HKZ8h-G-7cyhf-RRtfZTsi1OQaa4" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Home | Zentech</title>
+    <head profile="http://www.w3.org/2005/10/profile">
+      <link rel="icon" 
+            type="image/png" 
+            href="Images/Zentech Logo.png" />
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .carousel-item img {
+    position: absolute;
+    object-fit:cover;
+    top: 0;
+    left: 0;
+    min-height: 500px;
+}
+.Hrutvikimage{
+    object-fit: cover;
+  width: 500px;
+  height: -100px;
+}
+
+     
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="CSS/carousel.css" rel="stylesheet">
+    
+  </head>
+  <body>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.2.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+<script src="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.js"></script>
+<link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.css" />
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <a class="navbar-brand" href="#">Zentech</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="projects.html">Projects <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Youtube.html">YouTube</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Game.html">Saketh's Box Run</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Game2.html">Saketh's Ghost Run</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacts.html" tabindex="-1" aria-disabled="true">Contact Us</a>
+        </li>
+        <!--<li class="nav-item">
+          <a class="nav-link" href="birthday.html">Birthday Celebrations</a>
+        </li>-->
+        <li class="nav-item">
+          <a class="nav-link" href="dd.html">Art of CSS</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+          <!-- START THE FEATURETTES -->
+      
+          <hr class="featurette-divider">
+      
+          <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="Why we made">Today 10th February is Hrutvik's Birthday &#127881 &#127881.<br> He is the <strong>co-founder of Zentech inc.</strong> <span class="text-muted"></span></h2>
+                <p class="lead"></p>
+            </div>
+            <div class="col-md-5">
+              <img class="Hrutvikimage" id="image1" src="Hrutvik's Photos/1h.jpg" >
+              
+            </div>
+          </div>
+      
+          <hr class="featurette-divider">
+      
+          <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="Why we made">I would like to thank<strong> Hrutvik</strong> for all he has contributed to Zentech; His astonishing creativity which made us possible to make our website more attractive. He expanded the company by introducing new projects. I would like to thank him alot for his tremendous support.<span class="text-muted"></span></h2>
+                <p class="lead"></p>
+            </div>
+            <div class="col-md-5 order-md-1">
+              <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" id="image2" src="Hrutvik's Photos/2h.jpg" width="500px" height="500px">
+            </div>
+          </div>
+      
+          <hr class="featurette-divider">
+      
+          <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="Why we made">He is the man behind the name Zentech.<span class="text-muted"></span></h2>
+                
+            </div>
+            <div class="col-md-5">
+              <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" id="image2" src="Hrutvik's Photos/3h.jpg" width="500px" height="500px">
+            </div>
+          </div>
+          <hr class="featurette-divider">
+      
+          <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading"> Many many happy returns of the day, and hope you have a good year ahead! Happy birthday, Hrutvik.<span class="text-muted"></span></h2>
+            </div>
+            <div class="col-md-5 order-md-1">
+              <img class="Hrutvikimage" id="image2" src="Hrutvik's Photos/4h.jpg" width="500px" height="500px">
+            </div>
+          </div>
+      
+          <hr class="featurette-divider">
+      
+          <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading"><strong>Your Friend <br>-SAKETH</strong><span class="text-muted"></span></h2>
+               
+            </div>
+            <div class="col-md-5">
+              <img class="Hrutvikimage" id="image2" src="Hrutvik's Photos/5h.jpg" width="500px" height="500px">
+            </div>
+          </div>
+      
+      
+        <!-- FOOTER -->
+        <footer class="container">
+          <p class="float-right"><a href="#">Back to top</a></p>
+          <p>&copy; 2020-2020 Zentech Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+      </main>
+      <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    --></body>
+      </html>
+      
